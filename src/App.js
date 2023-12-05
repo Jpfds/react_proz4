c35534d2b0cef44bd05f05b28b21fcc0962eb0c2
Jpfds <jallinrabbei57@gmail.com>
2023-12-05 12:06:48 -0300

    atividade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Componente from './Component/Component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="divcomponent">
+      <Componente titulo="Seja bem vindo!!!" descricao="fique a vontade no site."></Componente>
     </div>
   );
 }
---
+++ src/Component/Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Componente = (props) => {
+    return (
+        <div class="divcomponent">
+            <h1>{props.titulo}</h1>
+            <p>{props.descricao}</p>
+        </div>
+    );
+}
+
+export default Componente;
